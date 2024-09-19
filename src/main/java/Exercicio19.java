5b4875d3c5e12190546ad517e2d81da775737586
root <root@DELL-7559>
2024-09-18 21:12:04 -0300

    Início do projeto

---
+++ src/main/java/Exercicio19.java
@@ -0,0 +1,26 @@
+// IMPORTS
+
+import java.util.Scanner;
+
+public class Exercicio19 {
+
+	public static void main(String[] args) {
+		// INSTANTIATE
+		Scanner sc = new Scanner(System.in);
+
+		// DECLARATE
+		int n;
+		String txt;
+
+		// COLLECT DATA
+		System.out.print("QT : ");
+		n = sc.nextInt();
+		System.out.print("TXT : ");
+		txt = sc.next();
+
+		// OUTPUT
+		for (int i = 0; i < n; i++) {
+			System.out.println(txt);
+		}
+	}
+}
