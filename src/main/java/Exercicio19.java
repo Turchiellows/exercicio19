e74c95f05ab223d0768eba89d53ebc053e66bd89
root <root@DELL-7559>
2024-09-28 14:24:49 -0300

    refactor: Mudança de indentação

--- src/main/java/Exercicio19.java
+++ src/main/java/Exercicio19.java
@@ -4,23 +4,23 @@ import java.util.Scanner;
 
 public class Exercicio19 {
 
-	public static void main(String[] args) {
-		// INSTANTIATE
-		Scanner sc = new Scanner(System.in);
+   public static void main(String[] args) {
+      // INSTANTIATE
+      Scanner sc = new Scanner(System.in);
 
-		// DECLARATE
-		int n;
-		String txt;
+      // DECLARATE
+      int n;
+      String txt;
 
-		// COLLECT DATA
-		System.out.print("QT : ");
-		n = sc.nextInt();
-		System.out.print("TXT : ");
-		txt = sc.next();
+      // COLLECT DATA
+      System.out.print("QT : ");
+      n = sc.nextInt();
+      System.out.print("TXT : ");
+      txt = sc.next();
 
-		// OUTPUT
-		for (int i = 0; i < n; i++) {
-			System.out.println(txt);
-		}
-	}
+      // OUTPUT
+      for (int i = 0; i < n; i++) {
+         System.out.println(txt);
+      }
+   }
 }
